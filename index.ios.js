8805a9141c385be959884f20ec1f8394b5e560b7
Alex Davis <alex2006hw@gmail.com>
2016-01-26 04:14:02 -0800

    added-components

--- index.ios.js
+++ index.ios.js
@@ -1,52 +1,41 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+var React = require('react-native');
 
-class findme extends Component {
-  render() {
+// Make react global
+window.React = React;
+
+var {
+  AppRegistry,
+  NavigatorIOS,
+  StyleSheet,
+} = React;
+
+var Login = require('./components/Login');
+var LoginMock = require('./components/LoginMock');
+
+var AppEntry = React.createClass({
+  render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        itemWrapperStyle={styles.allPages}
+        initialRoute={{
+          title: 'Login',
+          component: Login,
+        }}
+      />
     );
   }
-}
+});
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  allPages: {
+    backgroundColor: '#EEE',
   },
 });
 
-AppRegistry.registerComponent('findme', () => findme);
+AppRegistry.registerComponent('findme', () => AppEntry);
+
+module.exports = AppEntry;
